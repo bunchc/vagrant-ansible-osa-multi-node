0f186a3ce57fd9363573ffe8a404141f3298472c
bunchc <bunchc@gmail.com>
2018-07-18 16:56:00 -0500

    network interface duplication

--- network.sh
+++ network.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo "
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto ens3
+iface ens3 inet dhcp
+pre-up sleep 2" | tee /etc/network/interfaces
+
 # Down the interfaces
 ifdown eth1
 ifdown eth2
